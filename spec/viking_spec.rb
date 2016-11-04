2f27d55d0e58eade209d98e5f2c93af4ee02be57
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-04 11:56:48 -0700

    Add bow and some viking tests

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,1 +1,14 @@
-# Your code here
+require 'viking'
+
+describe Viking do
+
+  describe "initialization" do
+
+    it "Vikings can be named" do
+      sven = Viking.new("Sven")
+      expect(sven.instance_variable_get(:@name)).to eq("Sven")
+    end
+
+  end
+
+end
--- spec/weapons/bow_spec.rb
+++ spec/weapons/bow_spec.rb
@@ -1,1 +1,40 @@
-# Your code here
+require 'weapons/bow'
+
+describe Bow do
+
+  let(:test_bow) { Bow.new }
+
+  describe "initialization" do
+
+    it "arrow should be accessible" do
+      expect(test_bow).to respond_to(:arrows)
+    end
+
+    it "should have 10 arrows by default" do
+      test = Bow.new
+      expect(test.arrows).to eq(10)
+    end
+
+    it "should equal the number of arrows its given" do
+      test = Bow.new(20)
+      expect(test.arrows).to eq(20)
+    end
+
+  end
+
+  describe "#use" do
+
+    it "should reduce arrows by 1" do
+      test_bow.use
+      expect(test_bow.arrows).to eq(9)
+    end
+
+    it "should not shoot 0 arrows" do
+      test = Bow.new(0)
+      expect do
+        test.use
+      end.to raise_error("Out of arrows")
+    end
+
+  end
+end
