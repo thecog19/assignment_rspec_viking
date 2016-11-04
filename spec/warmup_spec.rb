ab7b7d463483ea0c07345a3e7f4ba7d150d23260
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-04 10:20:41 -0700

    Add name to readme, write tests for triple_size

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -3,15 +3,32 @@ require 'warmup'
 describe Warmup do
 
   let(:warm) {Warmup.new}
-  
+
   describe "#gets_shout" do
+
     it "shouts successfuly" do
       allow(warm).to receive(:gets).and_return("hello")
       expect(warm.gets_shout).to eq("HELLO")
     end
+
   end
 
-  describe "#triple_size" do 
-    it "does stuff"
+  describe "#triple_size" do
+
+    let(:arr_double) do
+      double("Array", :size => 2)
+    end
+
+    it "triples the size of an array" do
+      expect(warm.triple_size(arr_double)).to eq(6)
+    end
+
   end
+
+  describe "#calls_some_methods" do
+
+    it ""
+
+  end
+
 end
